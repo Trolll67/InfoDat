55f6f9f4b4282114a076ae7179ccdaa3c399c3bc
Serafim (unity developer) <serafim@bullyentertainment.com>
2023-09-29 03:05:27 +0300

    Revert "Fixed parsing boolean values"
    This reverts commit c9004c55f11fccfbc3fbcdace94c749d363f526e.

--- InfoDat/Database.cs
+++ InfoDat/Database.cs
@@ -140,7 +140,7 @@ public class Database
                     {
                         var val = reader.GetValue(i);
                         if (val is bool boolValue)
-                            val = boolValue ? "1" : "0";
+                            val = boolValue ? "0" : "1";
                         
                         dataItem.Add(reader.GetName(i), val.ToString());
                     }
